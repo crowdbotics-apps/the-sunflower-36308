1740e7ebc6a6b570ee20e480b07c6cdfb7332a24
Crowdbotics <team@crowdbotics.com>
2022-07-20 07:03:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "the_sunflower_36308",
+  url: "https://the_sunflower_36308.botics.co",
+  api: "https://the_sunflower_36308.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
